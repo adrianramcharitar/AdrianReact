f95101bd5ceeb8293dd44385b7bab532ceeb034c
adrianramcharitar <adrian.ramcharitar@focisolutions.com>
2019-05-25 12:34:58 -0400

    added fetch api for Unsplash

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = ''
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state={
+      photos: [],
+      term: ''
+    }
+  }
+
+  componentDidMount(){
+    if(this.state.term !== ""){
+      this.fetchPhotos(this.state.term)
+    }else{
+      this.fetchPhotos('react');
+    }
+  }
+
+  fetchPhotos = (term) => {
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${term}&client_id=${API_KEY}`)
+    .then(resp => {return resp.json() })
+    .then(data => {
+      this.setState({
+        photos: data.results
+      })
+      .catch(err => {
+        console.log('Fetching Error', err);
+      });
+    })
+  }
+
+  changeSearchTermState = (event) => {
+    this.setState({
+      term: event.target.value
+    }, () => {this.state.term === "" ? this.fetchPhotos('coding') : this.fetchPhotos(this.state.term)})
+  }
+
+
+  render() {
+    return(
+    <div>
+
+    </div>);
+  }
 }
-
 export default App;
